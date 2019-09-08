62ff95f2da469874121dbc6feece14a849ef8203
hungwei123 <hungwei123.cs06@g2.nctu.edu.tw>
2019-09-08 21:00:45 +0800

    general move between dimensions

--- routes/user/common/handler/graduate/course/restore.js
+++ routes/user/common/handler/graduate/course/restore.js
@@ -1,4 +1,5 @@
 var restore = {};
+var query = require('../../../../../../db/msql');
 
 restore.processRestore = function(req, res, next){
     
@@ -6,7 +7,7 @@ restore.processRestore = function(req, res, next){
         var studentId = res.locals.studentId;
         var courseResult = res.locals.courseResult;
         //console.log(courseResult[0]);
-
+        var allScore = JSON.parse(req.pass);
         var courses = req.changeCourses;
         courses = JSON.parse(courses);
         //console.log("change courses:");
@@ -15,7 +16,9 @@ restore.processRestore = function(req, res, next){
         var restoreIndex = [];
         var tempPre = [];
         var tempNext = [];
-        
+        var restorePreGeneralNew = [];
+        var restoreNextGeneralNew = [];
+
         for(var i = 0; i<11; i++){
             var course = {
                 pre:[],
@@ -49,9 +52,14 @@ restore.processRestore = function(req, res, next){
                 restore[3].pre[courses[i].cos_cname] = true;
                 restoreIndex[3].pre.push(courses[i].cos_cname);
             }
-            else if(courses[i].orig_pos.substring(0,2) == '通識'){
+            else if(courses[i].orig_pos.substring(0,6) == '通識(舊制)'){
                 restore[4].pre[courses[i].cos_cname] = true;
                 restoreIndex[4].pre.push(courses[i].cos_cname);
+            }
+            else if(courses[i].orig_pos.substring(0,6) == '通識(新制)'){
+                restore[5].pre[courses[i].cos_cname] = true;
+                restoreIndex[5].pre.push(courses[i].cos_cname);
+                restorePreGeneralNew[courses[i].cos_cname] = courses[i].orig_pos;
             }
             else if(courses[i].orig_pos == '體育'){
                 restore[6].pre[courses[i].cos_cname] = true;
@@ -89,9 +97,14 @@ restore.processRestore = function(req, res, next){
                 restore[3].next[courses[i].cos_cname] = true;
                 restoreIndex[3].next.push(courses[i].cos_cname);
             } 
-            else if(courses[i].now_pos.substring(0,2) == '通識'){
+            else if(courses[i].now_pos.substring(0,6) == '通識(舊制)'){
                 restore[4].next[courses[i].cos_cname] = true;
                 restoreIndex[4].next.push(courses[i].cos_cname);
+            }
+            else if(courses[i].now_pos.substring(0,6) == '通識(新制)'){
+                restore[5].next[courses[i].cos_cname] = true;
+                restoreIndex[5].next.push(courses[i].cos_cname);
+                restoreNextGeneralNew[courses[i].cos_cname] = courses[i].now_pos;
             }
             else if(courses[i].now_pos == '體育'){
                 restore[6].next[courses[i].cos_cname] = true;
@@ -119,49 +132,71 @@ restore.processRestore = function(req, res, next){
        //console.log(courseResult[6]);
        var general_old_dim = [];
        var general_new_dim = [];
+
        for(var i = 0; i<courseResult.length; i++){
-        if(i == 5) continue;
         for(var q = 0; q<courseResult[i].course.length; q++){
-           /*if(courseResult[i].course[q].code == 'DBT1103'){
-               console.log("i" + i+" p: "+q);
-               console.log(courseResult[i].course[q]);
-           }*/
             if(restore[i].pre[courseResult[i].course[q].cn] == true){
                restore[i].pre[courseResult[i].course[q].cn] = false;
-             /*  if(i == 3){
-                console.log("in first loop");
-                console.log("i:" + i + " p: " + q);
-                console.log(courseResult[i].course[q].code);
-               }*/
-                tempPre[courseResult[i].course[q].cn] = courseResult[i].course[q];
-                //console.log( tempPre[courseResult[i].course[q].code]); 
-                courseResult[i].credit -= parseFloat(courseResult[i].course[q].realCredit);
+
+               let cos = courseResult[i].course[q];
+               let cosName = cos.cn; 
+               for(let j = 0; j < allScore.length; j++){
+                    if(cosName == allScore[j].cos_cname){
+                        if(allScore[j].brief)
+                            general_old_dim[cosName] = allScore[j].brief.substring(0,2);
+                        if(allScore[j].brief_new)
+                            general_new_dim[cosName] = allScore[j].brief_new.substring(0, allScore[j].brief_new.length-5);
+                    }
+               }
+                // Move from old general
                 if(i == 4) {
-                    courseResult[5].credit.total -= parseFloat(courseResult[i].course[q].realCredit);
-                    /*if(courseResult[i].course[q].brief_new.substring(0,2) == '核心')
-                        courseResult[5].credit.core -= parseFloat(courseResult[i].course[q].realCredit);
-                    else if(courseResult[i].course[q].brief_new.substring(0,2) == '跨院')
-                        courseResult[5].credit.cross -= parseFloat(courseResult[i].course[q].realCredit);
-                    else if(courseResult[i].course[q].brief_new.substring(0,2) == '校基')
-                        courseResult[5].credit.basic -= parseFloat(courseResult[i].course[q].realCredit);
-                    */
-                    let gen_name = courseResult[i].course[q].cn 
+                    // new
+                    courseResult[i].credit -= parseFloat(courseResult[i].course[q].realCredit);
+                    tempPre[cosName] = cos;
+                    courseResult[i].course.splice(q,1);
+                    // old
+                    courseResult[5].credit.total -= parseFloat(cos.realCredit); 
+                    if(general_new_dim[cosName].substring(0,2) == '核心')
+                        courseResult[5].credit.core -= parseFloat(cos.realCredit);
+                    else if(general_new_dim[cosName].substring(0,2) == '跨院')
+                        courseResult[5].credit.cross -= parseFloat(cos.realCredit);
+                    else if(general_new_dim[cosName].substring(0,2) == '校基')
+                        courseResult[5].credit.basic -= parseFloat(cos.realCredit);
                     for(var k = 0; k < courseResult[5].course.length; k++){
-                        if(gen_name == courseResult[5].course[k].cn){
-                            general_old_dim[gen_name] = courseResult[i].course[q].dimension;
-                            general_new_dim[gen_name] = courseResult[5].course[k].dimension;
-                            if(courseResult[5].course[k].dimension.substring(0,2) == '核心')
-                                courseResult[5].credit.core -= parseFloat(courseResult[i].course[q].realCredit);
-                            else if(courseResult[5].course[k].dimension.substring(0,2) == '跨院')
-                                courseResult[5].credit.cross -= parseFloat(courseResult[i].course[q].realCredit);
-                            else if(courseResult[5].course[k].dimension.substring(0,2) == '校基')
-                                courseResult[5].credit.basic -= parseFloat(courseResult[i].course[q].realCredit);
+                        if(cosName == courseResult[5].course[k].cn){
+                            courseResult[5].course.splice(k,1);
                             break;
                         }
                     }
-                    courseResult[5].course.splice(k,1);
                 }
-                courseResult[i].course.splice(q,1);
+                // Move from new general
+                else if(i == 5) {
+                    // old
+                    courseResult[4].credit  -= parseFloat(cos.realCredit);
+                    for(var k = 0; k < courseResult[4].course.length; k++){
+                        if(cosName == courseResult[4].course[k].cn){
+                            courseResult[4].course.splice(k,1);
+                            break;
+                        }
+                    }
+                    // new
+                    courseResult[i].credit.total -= parseFloat(cos.realCredit);
+                    tempPre[cosName] = cos;
+                    courseResult[i].course.splice(q,1);
+                    let dim = restorePreGeneralNew[cosName].substring(7);
+                    if(dim.substring(0,2) == '核心')
+                        courseResult[i].credit.core -= parseFloat(cos.realCredit);
+                    else if(dim.substring(0,2) == '跨院')
+                        courseResult[i].credit.cross -= parseFloat(cos.realCredit);
+                    else if(dim.substring(0,2) == '校基')
+                        courseResult[i].credit.basic -= parseFloat(cos.realCredit);
+                }
+                // Move from other place
+                else{
+                    tempPre[courseResult[i].course[q].cn] = courseResult[i].course[q];
+                    courseResult[i].credit -= parseFloat(courseResult[i].course[q].realCredit);
+                    courseResult[i].course.splice(q,1);
+                }
                 q--;
             }
         }
@@ -169,37 +204,53 @@ restore.processRestore = function(req, res, next){
       // console.log(tempPre);
        //push the course the course in next to the new position
        for(var i = 0; i<courseResult.length; i++){
-        if(i == 5) continue;
         for(var q = 0; q<restoreIndex[i].next.length; q++){
-                //console.log("i:" + i + " q:" + q);
-                if(typeof(tempPre[restoreIndex[i].next[q]]) != undefined && tempPre[restoreIndex[i].next[q]] ){
+                let cosName = restoreIndex[i].next[q];
+                let cos = tempPre[cosName];
+                if(typeof(cos) != undefined && cos ){
                     if(i == 4){
-                        courseResult[i].credit += parseFloat(tempPre[restoreIndex[i].next[q]].realCredit);
-                        courseResult[5].credit.total += parseFloat(tempPre[restoreIndex[i].next[q]].realCredit);
-                        let temp_general_old = Object.assign({}, tempPre[restoreIndex[i].next[q]]);
-                        let temp_general_new = Object.assign({}, tempPre[restoreIndex[i].next[q]]);
-                        temp_general_old.dimension = general_old_dim[restoreIndex[i].next[q]];
+                        courseResult[i].credit += parseFloat(cos.realCredit);
+                        courseResult[5].credit.total += parseFloat(cos.realCredit);
+                        let temp_general_old = Object.assign({}, cos);
+                        let temp_general_new = Object.assign({}, cos);
+                        temp_general_old.dimension = general_old_dim[cosName];
                         if (temp_general_old.dimension == null) temp_general_old.dimension = '自然';
-                        temp_general_new.dimension = general_new_dim[restoreIndex[i].next[q]];
+                        temp_general_new.dimension = general_new_dim[cosName];
                         if (temp_general_new.dimension == null) temp_general_new.dimension = '校基本素養';
                         courseResult[i].course.push(temp_general_old);
                         courseResult[5].course.push(temp_general_new);
                         if(temp_general_new.dimension.substring(0,2) == '核心')
-                            courseResult[5].credit.core += parseFloat(courseResult[i].course[q].realCredit);
+                            courseResult[5].credit.core += parseFloat(cos.realCredit);
                         else if(temp_general_new.dimension.substring(0,2) == '跨院')
-                            courseResult[5].credit.cross += parseFloat(courseResult[i].course[q].realCredit);
+                            courseResult[5].credit.cross += parseFloat(cos.realCredit);
                         else if(temp_general_new.dimension.substring(0,2) == '校基')
-                            courseResult[5].credit.basic += parseFloat(courseResult[i].course[q].realCredit);
+                            courseResult[5].credit.basic += parseFloat(cos.realCredit);
+                    }
+                    else if(i == 5){
+                        // old general
+                        courseResult[4].credit += parseFloat(cos.realCredit);
+                        let temp_general_old = Object.assign({}, cos);
+                        temp_general_old.dimension = general_old_dim[cosName];
+                        if (temp_general_old.dimension == null) temp_general_old.dimension = '自然';
+                        courseResult[4].course.push(temp_general_old);
+                        // new general
+                        courseResult[5].credit.total += parseFloat(cos.realCredit);
+                        let temp_general_new = Object.assign({}, cos);
+                        temp_general_new.dimension = restoreNextGeneralNew[cosName].substring(7);
+                        if (temp_general_new.dimension == null) temp_general_new.dimension = '校基本素養';
+                        courseResult[i].course.push(temp_general_new);
+                        if(temp_general_new.dimension.substring(0,2) == '核心')
+                            courseResult[5].credit.core += parseFloat(cos.realCredit);
+                        else if(temp_general_new.dimension.substring(0,2) == '跨院')
+                            courseResult[5].credit.cross += parseFloat(cos.realCredit);
+                        else if(temp_general_new.dimension.substring(0,2) == '校基')
+                            courseResult[5].credit.basic += parseFloat(cos.realCredit);
                     }
                     else {
                         courseResult[i].course.push(tempPre[restoreIndex[i].next[q]]);
                         courseResult[i].credit += parseFloat(tempPre[restoreIndex[i].next[q]].realCredit);
                     }
                 }
-                //else{
-                    
-                  //  console.log(studentId + ": " + restoreIndex[i].next[q]);
-                //}
             }
         }
        res.locals.courseResult = courseResult;
