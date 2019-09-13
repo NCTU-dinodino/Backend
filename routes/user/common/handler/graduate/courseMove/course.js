77205d79bee995f2c138f97642da40fb7bd491ea
lspss9950101 <lspss9950101@gmail.com>
2019-09-14 00:19:33 +0800

    revise course move

--- routes/user/common/handler/graduate/courseMove/course.js
+++ routes/user/common/handler/graduate/courseMove/course.js
@@ -120,11 +120,10 @@ var General = {
 						destination = [];
 						destination.push('通識(舊制)-' + course.brief.split('/')[0]);
 						//console.log(course.brief_new.split(','));
-						let brief_new_list = course.brief_new.split(',');
-						for(var dim in brief_new_list){
+						course.brief_new.split(',').forEach((dim) => {
 							console.log('raw' + dim);
 							console.log('processed' + dim.substring(0, dim.length - 5));
-						}
+						});
 							//destination.push('通識(新制)-' + dim.substring(0, dim.length - 5));
 						//destination = '通識(舊制)-' + course.brief + '|通識(新制)-' + course.brief_new;
 				}
