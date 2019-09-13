d506492b982bb16745f1cc520ffbcee7d5d09073
lspss9950101 <lspss9950101@gmail.com>
2019-09-13 23:59:45 +0800

    revise course move

--- routes/user/common/handler/graduate/courseMove/course.js
+++ routes/user/common/handler/graduate/courseMove/course.js
@@ -119,6 +119,7 @@ var General = {
 					case '校':case '核':case'跨':
 						destination = [];
 						destination.push('通識(舊制)-' + course.brief.split('/')[0]);
+						console.log(course.brief_new);
 						for(let dim in course.brief_new.split(','))
 							destination.push('通識(新制)-' + dim.substring(0, dim.length - 5));
 						//destination = '通識(舊制)-' + course.brief + '|通識(新制)-' + course.brief_new;
