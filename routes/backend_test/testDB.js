9cc4020ad28b3035a22bf043c8c57e0d65b68bda
lspss9950101 <lspss9950101@gmail.com>
2019-11-26 17:45:11 +0800

    automatically fetch studentId from url

--- routes/backend_test/testDB.js
+++ routes/backend_test/testDB.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var query = require('../../db/msql');
 
 router.get('/testDB', (req, res) => {
-	let studentId = '';
+	let url = req.url;
+	url = url.split('/');
+	let studentId = url.pop();
 	query.ShowUserAllScore(studentId, (err, result) => {
 		result = JSON.parse(result);
 		console.log(result);
