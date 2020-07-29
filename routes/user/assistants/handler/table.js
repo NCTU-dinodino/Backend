a7c64795a576fd3097e65fc6d9cb3039b8ea9c4c
GinnyCosine <ginny1010377@gmail.com>
2020-07-29 19:12:43 +0800

    fixx4

--- routes/user/assistants/handler/table.js
+++ routes/user/assistants/handler/table.js
@@ -770,41 +770,40 @@ table.researchProfessorList = function(req, res, next) {
     })
     .then((result) => {
         let research_index = [];
-        // result.forEach((student) => {
-        result.forEach((students_of_techer) => {
-            students_of_techer.forEach((student) => {
-                console.log(1);
-                // teacher_list[0].accepted.projects.push({st:student});
-            });
-                if ((student.semester == year_semester) && ((student.first_second == req.body.first_second) || ( (student.first_second == '3') && (req.body.first_second == '1') ))) {
-                    if (research_index[student.research_title] == null){
-                        let project = {
-                            title: student.research_title,
-                            students : [],
-                        }
-                        let teacher_idx = teacher_index[student.teacher_id];
-                        research_index[student.research_title] = teacher_list[teacher_idx].accepted.projects.length;
-                        teacher_list[teacher_idx].accepted.projects.push(project);
+        let students_ = [];
+        result.forEach((student_of_techer) => {
+            students_ = [...students_, ...student_of_techer];
+        });
+        students_.forEach((student) => {
+            // teacher_list[0].accepted.projects.push({st:student});
+            if ((student.semester == year_semester) && ((student.first_second == req.body.first_second) || ( (student.first_second == '3') && (req.body.first_second == '1') ))) {
+                if (research_index[student.research_title] == null){
+                    let project = {
+                        title: student.research_title,
+                        students : [],
                     }
-
-                    let student_info = {
-                        id: student.student_id,
-                        name: student.sname,
-                        program: student.class_detail,
-                        semester: student.semester,
-                        first_second: student.first_second,
-                        status: student.status,
-                        add_status: student.add_status,
-                        score: student.score == null ? null : parseInt(student.score),
-                        comment: student.comment
-                    }
-                    let research_idx = research_index[student.research_title];
                     let teacher_idx = teacher_index[student.teacher_id];
-                    teacher_list[teacher_idx].accepted.projects[research_idx].students.push(student_info);
-                    if((student.add_status == 0) && (teacher_list[teacher_idx].accept_status == 0))
-                        teacher_list[teacher_idx].accept_status = 1;
+                    research_index[student.research_title] = teacher_list[teacher_idx].accepted.projects.length;
+                    teacher_list[teacher_idx].accepted.projects.push(project);
                 }
-            // });
+
+                let student_info = {
+                    id: student.student_id,
+                    name: student.sname,
+                    program: student.class_detail,
+                    semester: student.semester,
+                    first_second: student.first_second,
+                    status: student.status,
+                    add_status: student.add_status,
+                    score: student.score == null ? null : parseInt(student.score),
+                    comment: student.comment
+                }
+                let research_idx = research_index[student.research_title];
+                let teacher_idx = teacher_index[student.teacher_id];
+                teacher_list[teacher_idx].accepted.projects[research_idx].students.push(student_info);
+                if((student.add_status == 0) && (teacher_list[teacher_idx].accept_status == 0))
+                    teacher_list[teacher_idx].accept_status = 1;
+            }
         });
 
         return Promise.all(promiseList_apply);
